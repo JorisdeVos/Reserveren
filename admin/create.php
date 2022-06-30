a3c8d4a6135fec2579aa10573ff7e91645f3af90
calthan leander <calsonleander99@gmail.com>
2022-06-30 12:48:58 +0200

    added create page

--- admin/create.php
+++ admin/create.php
@@ -22,7 +22,34 @@ session_start();
 		<?php require_once'../header.php'; ?>
 	</header>
 	<main>
-		
+		<form action="../backend/tasksController.php" method="POST">
+            <div class="editForm">
+                <input type="hidden" name="action" value="create">
+                <div class="question">
+                    <label for="name">naam:</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div class="question">
+                    <label for="description">beschrijving:</label>
+                    <textarea type="text" cols="50" rows="4" name="description" id="description"></textarea>
+                </div>
+                <div class="question">
+                    <label for="price">prijs:</label>
+                    <input type="number" name="price" id="price"/>
+                </div>
+                <div class="question">
+                    <label for="address">adres:</label>
+                    <input type="text" name="address" id="address">
+                </div>
+                <div class="question">
+                    <label for="image">foto naam:</label>
+                    <input type="text" name="image" id="image">
+                </div>
+                <div class="question">
+                <input type="submit" value="create">
+                </div>
+            </div>
+        </form>
 	</main>
 	<footer>
 		<?php require_once'../footer.php'; ?>
