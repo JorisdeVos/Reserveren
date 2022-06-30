7557f1451b8987347d41ff5c3def0ea0e452cf49
calthan leander <calsonleander99@gmail.com>
2022-06-30 12:12:18 +0200

    added top margin to admin page

--- admin/admin.php
+++ admin/admin.php
@@ -21,25 +21,27 @@ session_start();
         <?php require_once'../header.php'; ?>
     </header>
     <main>
-    <div class="allHouses">
-        <?php
-            require_once '../backend/conn.php';
-            $query = "SELECT * FROM houses";
-            $statement = $conn->prepare($query);
-            $statement->execute();
-            $houses = $statement->fetchALL(PDO::FETCH_ASSOC);
-        ?>
-        <?php foreach($houses as $house): ?>
-            <div class="house">
-                <img src="../<?php echo $house["image"]; ?>">
-                <div class="housesplit">
-                    <b><?php echo $house["name"]; ?></b>
-                    <p><?php echo $house["description"]; ?></p>
-                    <p><?php echo $house["price"]; ?>$ / Per dag</p>
-                    <button><a href="<?php echo $base_url; ?>/admin/edit.php?id=<?php echo $house["id"];?>">Edit</a></button>
+    <div class="container">
+        <div class="allHouses">
+            <?php
+                require_once '../backend/conn.php';
+                $query = "SELECT * FROM houses";
+                $statement = $conn->prepare($query);
+                $statement->execute();
+                $houses = $statement->fetchALL(PDO::FETCH_ASSOC);
+            ?>
+            <?php foreach($houses as $house): ?>
+                <div class="house">
+                    <img src="../<?php echo $house["image"]; ?>">
+                    <div class="housesplit">
+                        <b><?php echo $house["name"]; ?></b>
+                        <p><?php echo $house["description"]; ?></p>
+                        <p><?php echo $house["price"]; ?>$ / Per dag</p>
+                        <button><a href="<?php echo $base_url; ?>/admin/edit.php?id=<?php echo $house["id"];?>">Edit</a></button>
+                    </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
     </main>
     <footer>
