88424eff03205116173e3d7b54a139d776a9d931
calthan leander <calsonleander99@gmail.com>
2022-07-06 11:15:59 +0200

    changed header

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <?php require_once 'backend/config.php'; ?>
+<?php ?>
+
 
 <header>
 	<div class="headerNav">
@@ -6,7 +8,16 @@
 		<nav>
 			<a href="<?php echo $base_url; ?>/allHouses.php">All houses</a> |
 			<a href="<?php echo $base_url; ?>/index.php">Home</a> |
-			<a href="<?php echo $base_url; ?>/login.php">Login</a>
+			<?php
+            error_reporting(E_ERROR | E_WARNING | E_PARSE);
+            session_start(); 
+            if(!isset($_SESSION['user_id'])): ?>
+				<a href="<?php echo $base_url; ?>/login.php">Login</a>
+			<?php else: ?>
+                <?php echo "<p>Welkom,</p> <p><strong>", $_SESSION['name'], "|</strong></p>"?>
+                <a href="<?php echo $base_url; ?>/admin/admin.php" style="color: black">admin</a>
+                <a href="<?php echo $base_url; ?>/logout.php" style="color: black">Uitloggen</a>
+            <?php endif; ?>
 		</nav>
 	</div>
 </header>
